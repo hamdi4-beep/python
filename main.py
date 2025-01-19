2fc382f17ac82e6220bfd6f9c39ce6de60b4fd34
hamdi <hamdiakippuden@gmail.com>
2025-01-19 18:35:11 +0100

    added a button widget

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from tkinter import *
 from tkinter import ttk
 
+# creates the parent window
 root = Tk()
 root.title('Python Application')
+# sets the window's width and height
 root.geometry('800x600')
 
-frame = ttk.Frame(root, padding=20)
+# creates the frame
+frame = ttk.Frame(root, padding=10)
+# registers the frame with the grid manager
 frame.grid()
+
+# creates a button widget
+btn = ttk.Button(root, text='Quit application', command=root.destroy)
+btn.grid()
 
 root.mainloop()
