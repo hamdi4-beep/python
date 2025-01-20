ac42116a55cc5c4bb4bf7b3cce171e72b56e7756
hamdi <hamdiakippuden@gmail.com>
2025-01-20 08:21:57 +0100

    working with tkinter

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
-import pyautogui
+from tkinter import *
+from tkinter import ttk
 
-# opens up the search bar
-pyautogui.hotkey('win', 's')
+class Application(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack()
 
-# types in Notepad
-pyautogui.write('Notepad')
+        self.label = ttk.Label(text='Message:')
+        self.label.pack()
 
-# launch the app
-pyautogui.press('enter')
+root = Tk()
+root.title('Python GUI')
+root.geometry('800x600')
 
-# delays a few seconds before it starts writing into the file
-pyautogui.sleep(3)
+Application(root)
 
-# writes something into the app
-pyautogui.typewrite('Whoa! I can launch and use apps without having to do that manually.')
-
-# selects everything that was written
-pyautogui.hotkey('ctrl', 'a')
-
-# waits a few seconds before deleting selected text
-pyautogui.sleep(4)
-pyautogui.press('backspace')
-
-# exists the notepad
-pyautogui.hotkey('alt', 'f4')
+root.mainloop()
