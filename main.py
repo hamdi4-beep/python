9f8468c2a29b2428b9fb7b0b4e24df709235a51b
hamdi <hamdiakippuden@gmail.com>
2025-01-21 15:19:48 +0100

    wrote a script that retreives image links

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from urllib.request import Request, urlopen
 from html.parser import HTMLParser
 
+f = open('images.txt', 'w+')
+URL = 'https://en.wikipedia.org/wiki/PlayStation_4'
+
 class Parser(HTMLParser):
     def __init__(self, *, convert_charrefs = True):
         super().__init__(convert_charrefs=convert_charrefs)
-        self.isWithinSelectedTag = False
-    
-    def handle_starttag(self, tag, attrs):
-        for key, value in attrs:
-            if key == 'class' and value == 'sphinxsidebarwrapper':
-                self.isWithinSelectedTag = True
 
-    def handle_data(self, data):
-        if self.isWithinSelectedTag and data.strip():
-            print(data)
+    def handle_startendtag(self, tag, attrs):
+        if tag != 'img':
+            return
+        
+        for key, value in attrs:
+            if not value.startswith('/'):
+                return
+            
+            f.write(f'https:{value}\n')
+
+        f.write('\n')
 
 parser = Parser()
 
-with urlopen('https://docs.python.org/3/library/html.parser.html') as response:
+with urlopen(Request(URL)) as response:
     data = response.read()
     parser.feed(data.decode())
+
+f.close()
