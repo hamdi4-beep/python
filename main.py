7c14371de64359678ec272434492bb603774c5c1
hamdi <hamdiakippuden@gmail.com>
2025-01-27 13:23:57 +0100

    added commenting for better readability

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import sqlite3
 
+# creates a database connection
 conn = sqlite3.connect(':memory:')
+
+# creates a cursor object
 curr = conn.cursor()
 
-curr.execute("CREATE TABLE user (username, id)")
+# creates a user table
+curr.execute("CREATE TABLE user (username, age)")
 
+# insert values into the table
 curr.execute(
     """
         INSERT INTO user VALUES
-            ('hamdi4-beep', 8)
+            ('hamdi4-beep', 27)
     """
 )
 
+# commits the changes to the database
 conn.commit()
 
-res = curr.execute("SELECT username, id FROM user")
-print(res.fetchall())
+res = curr.execute("SELECT username, age FROM user")
+
+# retreives values from the database
+username, age = res.fetchone()
+print(f'{username} is {age} years old!')
+
+conn.close()
