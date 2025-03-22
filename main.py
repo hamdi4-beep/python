ac2bd3344be7fee6f07b9c040f26388f0f927495
hamdi4-beep <hamdiakippuden@gmail.com>
2025-03-22 13:39:59 +0100

    added an exception block for keyboard interruption

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class Request(BaseHTTPRequestHandler):
+PORT = 3000
+
+class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.send_header('Content-Length', '2')
         self.end_headers()
-        self.wfile.write(b'Hello, world!')
+
+        self.wfile.write(b'OK')
+
+server = HTTPServer(('localhost', PORT), RequestHandler)
 
 try:
-    httpd = HTTPServer(('', 3000), Request)
-    httpd.serve_forever()
-except Exception as e:
-    print('Something went wrong!', e)
+    print("Server started on port " + str(PORT))
+    server.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+server.server_close()
+print("Server stopped.")
