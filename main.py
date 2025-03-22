88de3dd312fd2a294f30f10b20213c3666be7471
hamdi4-beep <hamdiakippuden@gmail.com>
2025-03-22 13:47:43 +0100

    added a handler for post request

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ PORT = 3000
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
+
         self.send_header('Content-Type', 'text/plain')
         self.send_header('Content-Length', '2')
         self.end_headers()
 
         self.wfile.write(b'OK')
+
+    def do_POST(self):
+        self.send_response(200)
+        self.end_headers()
+
+        data = self.rfile.read().decode()
+        print(f'Received: {data}')
 
 server = HTTPServer(('localhost', PORT), RequestHandler)
 
