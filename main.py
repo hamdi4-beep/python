653b2775fe63373db87445528c5355d9b73b7f99
hamdi4-beep <hamdiakippuden@gmail.com>
2025-03-29 16:21:03 +0100

    experimenting with socket connection

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import socket
 
-PORT = 3000
+s = socket.socket()
+s.bind(('', 8000))
 
-class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
+s.listen()
 
-        self.send_header('Content-Type', 'text/plain')
-        self.send_header('Content-Length', '2')
-        self.end_headers()
+print('Awaiting connections from the Node.js server...')
 
-        self.wfile.write(b'OK')
+while True:
+    conn, address = s.accept()
+    print(f'Connected to {address}')
 
-    def do_POST(self):
-        self.send_response(200)
-        self.end_headers()
-
-        data = self.rfile.read().decode()
-        print(f'Received: {data}')
-
-server = HTTPServer(('localhost', PORT), RequestHandler)
-
-try:
-    print("Server started on port " + str(PORT))
-    server.serve_forever()
-except KeyboardInterrupt:
-    pass
-
-server.server_close()
-print("Server stopped.")
+    try:
+        data = conn.send(b'The socket connection works as expected!')
+        conn.close()
+    except Exception:
+        print('Something went wrong!')
